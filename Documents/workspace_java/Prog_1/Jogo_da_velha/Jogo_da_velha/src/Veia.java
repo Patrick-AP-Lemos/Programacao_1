76d0283c7b3118e119672c85c2cb1321ec681869
Patrick-AP-Lemos <patrickpinheiro2830@gmail.com>
2026-04-01 03:18:44 -0300

    Primeira otimização do Jogo

--- Documents/workspace_java/Prog_1/Jogo_da_velha/Jogo_da_velha/src/Veia.java
+++ Documents/workspace_java/Prog_1/Jogo_da_velha/Jogo_da_velha/src/Veia.java
@@ -11,13 +11,6 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-/**
- * Programa do Jogo da Velha
- * Valendo pela 4ª Nota da disciplina de Programacao I
- * Equipe:
- * 1)
- * 2) 
- */
 /*
  * Veia extends JFrame: A classe Veia herda de JFrame
  * Veia implements ActionListener: A classe Veia implementa a interface ActionListener
@@ -56,60 +49,25 @@ public class Veia extends JFrame implements ActionListener {
 		
 		container = getContentPane();
 		container.setLayout( null );
-		
-		botao[0][0] = new JButton();
-		botao[0][0].setBounds( 40, 40, 80, 80 );
-		botao[0][0].setActionCommand( "botao00" );
-		botao[0][0].addActionListener( this );
-		
-		botao[0][1] = new JButton();
-		botao[0][1].setBounds( 150, 40, 80, 80 );
-		botao[0][1].setActionCommand( "botao01" );
-		botao[0][1].addActionListener( this );
 
-		botao[0][2] = new JButton();
-		botao[0][2].setBounds( 260, 40, 80, 80 );
-		botao[0][2].setActionCommand( "botao02" );
-		botao[0][2].addActionListener( this );
-		
-		botao[1][0] = new JButton();
-		botao[1][0].setBounds( 40, 150, 80, 80 );
-		botao[1][0].setActionCommand( "botao10" );
-		botao[1][0].addActionListener( this );
-		
-		botao[1][1] = new JButton();
-		botao[1][1].setBounds( 150, 150, 80, 80 );
-		botao[1][1].setActionCommand( "botao11" );
-		botao[1][1].addActionListener( this );
-
-		botao[1][2] = new JButton();
-		botao[1][2].setBounds( 260, 150, 80, 80 );
-		botao[1][2].setActionCommand( "botao12" );
-		botao[1][2].addActionListener( this );
-		
-		botao[2][0] = new JButton();
-		botao[2][0].setBounds( 40, 260, 80, 80 );
-		botao[2][0].setActionCommand( "botao20" );
-		botao[2][0].addActionListener( this );
-		
-		botao[2][1] = new JButton();
-		botao[2][1].setBounds( 150, 260, 80, 80 );
-		botao[2][1].setActionCommand( "botao21" );
-		botao[2][1].addActionListener( this );
-
-		botao[2][2] = new JButton();
-		botao[2][2].setBounds( 260, 260, 80, 80 );
-		botao[2][2].setActionCommand( "botao22" );
-		botao[2][2].addActionListener( this );
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				botao[i][j] = new JButton();
+				botao[i][j].setBounds(40 + j * 110, 40 + i * 110, 80, 80);
+				botao[i][j].setActionCommand("botao" + i + j);
+				botao[i][j].addActionListener(this);
+				container.add(botao[i][j]);
+			}
+		}
 		
 		novoJogo = new JButton( "Novo Jogo" );
 		novoJogo.setBounds( 40, 370, 120, 40 );
 		novoJogo.setActionCommand( "novoJogo" );
 		novoJogo.addActionListener( this );
 
-		sugestao = new JButton( "Sugestao" );
+		sugestao = new JButton( "Auto" );
 		sugestao.setBounds( 220, 370, 120, 40 );
-		sugestao.setActionCommand( "sugestao" );
+		sugestao.setActionCommand( "Auto" );
 		sugestao.addActionListener( this );
 
 		container.add( botao[0][0] );
@@ -130,218 +88,72 @@ public class Veia extends JFrame implements ActionListener {
 		setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
 	}
 
-	public void actionPerformed(ActionEvent arg0) {
-		
-		if( arg0.getActionCommand().equals( "novoJogo" ) ) {
-			novoJogo();
+	public void actionPerformed(ActionEvent e) {
+		String cmd = e.getActionCommand();
 
-		} else if ( arg0.getActionCommand().equals( "botao00" ) ) {
-			
-			if ( matrizConteudo[0][0] != 0 ) {
-				JOptionPane.showMessageDialog(estaClasse, "Botão já está marcado!" );
-			} else {
-				if ( quemJogaAgora == XIS ) {
-					quemJogaAgora = BOLA;
-					matrizConteudo[0][0] = XIS;
-					botao[0][0].setIcon( (Icon) xis );
-				} else {
-					quemJogaAgora = XIS;
-					matrizConteudo[0][0] = BOLA;
-					botao[0][0].setIcon( (Icon) bola );
-				}
-				analisaSeAlguemVenceuOuSeDeuVelha();
-			}
-			
-		} else if ( arg0.getActionCommand().equals( "botao01" ) ) {
-			
-			if ( matrizConteudo[0][1] != 0 ) {
-				JOptionPane.showMessageDialog(estaClasse, "Botão já está marcado!" );
-			} else {
-				if ( quemJogaAgora == XIS ) {
-					quemJogaAgora = BOLA;
-					matrizConteudo[0][1] = XIS;
-					botao[0][1].setIcon( (Icon) xis );
-				} else {
-					quemJogaAgora = XIS;
-					matrizConteudo[0][1] = BOLA;
-					botao[0][1].setIcon( (Icon) bola );
-				}
-				analisaSeAlguemVenceuOuSeDeuVelha();
-			}
-			
-		} else if ( arg0.getActionCommand().equals( "botao02" ) ) {
-			
-			if ( matrizConteudo[0][2] != 0 ) {
-				JOptionPane.showMessageDialog(estaClasse, "Botão já está marcado!" );
-			} else {
-				if ( quemJogaAgora == XIS ) {
-					quemJogaAgora = BOLA;
-					matrizConteudo[0][2] = XIS;
-					botao[0][2].setIcon( (Icon) xis );
-				} else {
-					quemJogaAgora = XIS;
-					matrizConteudo[0][2] = BOLA;
-					botao[0][2].setIcon( (Icon) bola );
-				}
-				analisaSeAlguemVenceuOuSeDeuVelha();
-			}
-			
-		} else if ( arg0.getActionCommand().equals( "botao10" ) ) {
-			
-			if ( matrizConteudo[1][0] != 0 ) {
-				JOptionPane.showMessageDialog(estaClasse, "Botão já está marcado!" );
-			} else {
-				if ( quemJogaAgora == XIS ) {
-					quemJogaAgora = BOLA;
-					matrizConteudo[1][0] = XIS;
-					botao[1][0].setIcon( (Icon) xis );
-				} else {
-					quemJogaAgora = XIS;
-					matrizConteudo[1][0] = BOLA;
-					botao[1][0].setIcon( (Icon) bola );
-				}
-				analisaSeAlguemVenceuOuSeDeuVelha();
-			}
-			
-		} else if ( arg0.getActionCommand().equals( "botao11" ) ) {
-			
-			if ( matrizConteudo[1][1] != 0 ) {
-				JOptionPane.showMessageDialog(estaClasse, "Botão já está marcado!" );
-			} else {
-				if ( quemJogaAgora == XIS ) {
-					quemJogaAgora = BOLA;
-					matrizConteudo[1][1] = XIS;
-					botao[1][1].setIcon( (Icon) xis );
-				} else {
-					quemJogaAgora = XIS;
-					matrizConteudo[1][1] = BOLA;
-					botao[1][1].setIcon( (Icon) bola );
-				}
-				analisaSeAlguemVenceuOuSeDeuVelha();
-			}
-			
-		} else if ( arg0.getActionCommand().equals( "botao12" ) ) {
-			
-			if ( matrizConteudo[1][2] != 0 ) {
-				JOptionPane.showMessageDialog(estaClasse, "Botão já está marcado!" );
-			} else {
-				if ( quemJogaAgora == XIS ) {
-					quemJogaAgora = BOLA;
-					matrizConteudo[1][2] = XIS;
-					botao[1][2].setIcon( (Icon) xis );
-				} else {
-					quemJogaAgora = XIS;
-					matrizConteudo[1][2] = BOLA;
-					botao[1][2].setIcon( (Icon) bola );
-				}
-				analisaSeAlguemVenceuOuSeDeuVelha();
-			}
-			
-		} else if ( arg0.getActionCommand().equals( "botao20" ) ) {
-			
-			if ( matrizConteudo[2][0] != 0 ) {
-				JOptionPane.showMessageDialog(estaClasse, "Botão já está marcado!" );
-			} else {
-				if ( quemJogaAgora == XIS ) {
-					quemJogaAgora = BOLA;
-					matrizConteudo[2][0] = XIS;
-					botao[2][0].setIcon( (Icon) xis );
-				} else {
-					quemJogaAgora = XIS;
-					matrizConteudo[2][0] = BOLA;
-					botao[2][0].setIcon( (Icon) bola );
-				}
-				analisaSeAlguemVenceuOuSeDeuVelha();
-			}
-			
-		} else if ( arg0.getActionCommand().equals( "botao21" ) ) {
-			
-			if ( matrizConteudo[2][1] != 0 ) {
-				JOptionPane.showMessageDialog(estaClasse, "Botão já está marcado!" );
-			} else {
-				if ( quemJogaAgora == XIS ) {
-					quemJogaAgora = BOLA;
-					matrizConteudo[2][1] = XIS;
-					botao[2][1].setIcon( (Icon) xis );
-				} else {
-					quemJogaAgora = XIS;
-					matrizConteudo[2][1] = BOLA;
-					botao[2][1].setIcon( (Icon) bola );
-				}
-				analisaSeAlguemVenceuOuSeDeuVelha();
-			}
-			
-		} else if ( arg0.getActionCommand().equals( "botao22" ) ) {
-			
-			if ( matrizConteudo[2][2] != 0 ) {
-				JOptionPane.showMessageDialog(estaClasse, "Botão já está marcado!" );
-			} else {
-				if ( quemJogaAgora == XIS ) {
-					quemJogaAgora = BOLA;
-					matrizConteudo[2][2] = XIS;
-					botao[2][2].setIcon( (Icon) xis );
-				} else {
-					quemJogaAgora = XIS;
-					matrizConteudo[2][2] = BOLA;
-					botao[2][2].setIcon( (Icon) bola );
-				}
-				analisaSeAlguemVenceuOuSeDeuVelha();
-			}
-			
-		} else if ( arg0.getActionCommand().equals( "sugestao" ) ){
+		if (cmd.equals("novoJogo")) {
+			novoJogo();
+		} else if (cmd.equals("Auto")) {
 			sugestao();
+		} else if (cmd.startsWith("botao")) {
+			int i = Character.getNumericValue(cmd.charAt(5));
+			int j = Character.getNumericValue(cmd.charAt(6));
+			handleBotaoClick(i, j);
 		}
 	}
+
+	private void handleBotaoClick(int i, int j) {
+		if (matrizConteudo[i][j] != VAZIO) {
+			JOptionPane.showMessageDialog(this, "Botão já está marcado!");
+			return;
+		}
+		if (quemJogaAgora == XIS) {
+			matrizConteudo[i][j] = XIS;
+			botao[i][j].setIcon(xis);
+			quemJogaAgora = BOLA;
+		} else {
+			matrizConteudo[i][j] = BOLA;
+			botao[i][j].setIcon(bola);
+			quemJogaAgora = XIS;
+		}
+		analisaSeAlguemVenceuOuSeDeuVelha();
+	}
 	
+	private static final int[][] COMBINACOES = {
+		{0,0, 0,1, 0,2}, // linha 0
+		{1,0, 1,1, 1,2}, // linha 1
+		{2,0, 2,1, 2,2}, // linha 2
+		{0,0, 1,0, 2,0}, // coluna 0
+		{0,1, 1,1, 2,1}, // coluna 1
+		{0,2, 1,2, 2,2}, // coluna 2
+		{0,0, 1,1, 2,2}, // diagonal primária
+		{0,2, 1,1, 2,0}  // diagonal secundária
+	};
+
+	private int verificaVencedor() {
+		for (int[] c : COMBINACOES) {
+			int a = matrizConteudo[c[0]][c[1]];
+			int b = matrizConteudo[c[2]][c[3]];
+			int d = matrizConteudo[c[4]][c[5]];
+			if (a != VAZIO && a == b && b == d) return a;
+		}
+		return VAZIO;
+	}
+
 	/**
 	 * Esta função é a que é o trabalho (0-9,5 pontos)
 	 */
 	private void analisaSeAlguemVenceuOuSeDeuVelha() {
-		cont ++;
-		
-		for(int i=0, j=0; i<3; i++, j++) {//=============== JOGADOR X ===============
-			if(matrizConteudo[i][0] == XIS && matrizConteudo[i][1] == XIS && matrizConteudo[i][2] == XIS) {//Horizontal
-				JOptionPane.showMessageDialog(estaClasse, "Jogador X \nGANHOOUU" );
-				novoJogo();
-			}
-			if(matrizConteudo[0][j] == XIS && matrizConteudo[1][j] == XIS && matrizConteudo[2][j] == XIS) {//Vertical
-				JOptionPane.showMessageDialog(estaClasse, "Jogador X \nGANHOOUU" );
-				novoJogo();
-			}
-			if(matrizConteudo[0][0] == XIS && matrizConteudo[1][1] == XIS && matrizConteudo[2][2] == XIS) {//Diagonal Primária
-				JOptionPane.showMessageDialog(estaClasse, "Jogador X \nGANHOOUU" );
-				novoJogo();
-			}
-			if(matrizConteudo[0][2] == XIS && matrizConteudo[1][1] == XIS && matrizConteudo[2][0] == XIS) {//Diagonal Secundária
-				JOptionPane.showMessageDialog(estaClasse, "Jogador X \nGANHOOUU" );
-				novoJogo();
-			}
-		}
-		
-		for(int i=0, j=0; i<3; i++, j++) {//=============== JOGADOR O ===============
-			if(matrizConteudo[i][0] == BOLA && matrizConteudo[i][1] == BOLA && matrizConteudo[i][2] == BOLA) {//Horizontal
-				JOptionPane.showMessageDialog(estaClasse, "Jogador O \nGANHOOUU" );
-				novoJogo();
-			}
-			if(matrizConteudo[0][j] == BOLA && matrizConteudo[1][j] == BOLA && matrizConteudo[2][j] == BOLA) {//Vertical
-				JOptionPane.showMessageDialog(estaClasse, "Jogador O \nGANHOOUU" );
-				novoJogo();
-			}
-			if(matrizConteudo[0][0] == BOLA && matrizConteudo[1][1] == BOLA && matrizConteudo[2][2] == BOLA) {//Diagonal Primária
-				JOptionPane.showMessageDialog(estaClasse, "Jogador O \nGANHOOUU" );
-				novoJogo();
-			}
-			if(matrizConteudo[0][2] == BOLA && matrizConteudo[1][1] == BOLA && matrizConteudo[2][0] == BOLA) {//Diagonal Secundária
-				JOptionPane.showMessageDialog(estaClasse, "Jogador O \nGANHOOUU" );
-				novoJogo();
-			}
-		}
-		
-		if(cont == 9) {//=============== DEU VELHA ===============
-			JOptionPane.showMessageDialog(estaClasse, "DEU VELHA" );
+		cont++;
+		int vencedor = verificaVencedor();
+		if (vencedor != VAZIO) {
+			String nome = (vencedor == XIS) ? "X" : "O";
+			JOptionPane.showMessageDialog(this, "Jogador " + nome + "\nGANHOUU!");
+			novoJogo();
+		} else if (cont == 9) {
+			JOptionPane.showMessageDialog(this, "DEU VELHA!");
 			novoJogo();
 		}
-		
 	}
 	
 	/**
@@ -688,5 +500,3 @@ public class Veia extends JFrame implements ActionListener {
 	}
 	
 }
-
-
